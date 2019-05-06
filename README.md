6dd90473416d21b80386f334fac1c8057977a06a
Dimitris Tsolis <dimitris.re@gmail.com>
2019-05-06 21:17:39 +0300

    Adding readme and final changes on build.sh

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# log-events-tool
+----------TOOLS USED----------
+kafka 2.2.0 for Scala 2.11--> https://www.apache.org/dyn/closer.cgi?path=/kafka/2.2.0/kafka_2.11-2.2.0.tgz
+Cassandra 3.11.4 --> http://cassandra.apache.org/download/
+cqlsh 5.0.1
+maven 3.6.0 --> 
+java 1.8.0_212 - java-8-openjdk-amd64
+Thrift version 0.9.1
+Scala 2.11.8
+
+
+-----------INSTRUCTIONS---------
+To install apache kafka follow these instructions : https://www.digitalocean.com/community/tutorials/how-to-install-apache-kafka-on-ubuntu-18-04
+To install apache Cassandra follow the first section of instructions for Debian systems: http://cassandra.apache.org/download/
+To install maven3 follow these instructions https://maven.apache.org/install.html
+To install thrift follow these instructions https://thrift.apache.org/docs/install/ (In a Debian environment just sudo apt install thrift-compiler)
+
+PROS: 1. you have to have a working instance of cassandra database
+      2. you have to have working instances of zookeeper and kafka 
+
+1. Clone the git repository from https://github.com/dimitrisre/log-events-tool.git
+2. Change directory to log-events-tool
+3. run build.sh -b to build the project
+4. run build.sh -r to run the project
+5. run build.sh -s to stop modules 
+
+You can check the logs inside the logs directory with:
+tail -f kafka-consumer.log 
+tail -f thrift-server.log
+tail -f thrift-client.log
+
+To generate more log messages you can run 
+touch generate_random_logs.tmp
+while the project modules are runnning
--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+KAFKA_HOME="/home/kafka/kafka"
+
 function show_help {
+cclienkafkaconsumerEOF
 cat << EOF
     -b  build all the modules
     -r  run all the modules
+    -s  to stop all modules
     -h  this help text
+
+if you have install kafka in a different location than $KAFKA_HOME please pass an env variable before this script eg. KAFKA_HOME=/opt/kafka ./build.sh -r
 EOF
 }
 
 build=0
 run=0
+stopApps=0
 while true; do
     case "$1" in
         -h)
@@ -24,6 +31,10 @@ while true; do
             run=1
             shift
             ;;
+         -s)
+			stopApps=1
+			shift
+			;;
          *)
             shift
             break
@@ -31,7 +42,8 @@ while true; do
      esac
 done
 
-echo "build=$build run=$run"
+echo "build=$build run=$run stopApps=$stopApps KAFKA_HOME=$KAFKA_HOME"
+
 if [ "$build" -eq "1" ]; then
     mvn clean install && \
     cp thrift-server/target/thrift-server-1.0-SNAPSHOT-jar-with-dependencies.jar bin/ && \
@@ -41,9 +53,23 @@ if [ "$build" -eq "1" ]; then
 fi
 
 if [ "$run" -eq "1" ]; then
-    scala -classpath bin/thrift-server-1.0-SNAPSHOT-jar-with-dependencies.jar com.interview.thriftserver.ThriftServerApp &> logs/thrift-server.log &
+	touch ~/generate_random_logs.tmp
+	$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic LogEventsTopic
+	cqlsh -f common/src/main/resources/schema.cql
+	echo "Sleeping for 5 seconds" && sleep 5
+    java -classpath bin/thrift-server-1.0-SNAPSHOT-jar-with-dependencies.jar com.interview.thriftserver.ThriftServerApp &> logs/thrift-server.log &
+    echo "$!" > thriftserver.pid
     echo 'Sleeping for 5 seconds' && sleep 5
-    scala -classpath bin/kafka-consumer-1.0-SNAPSHOT-jar-with-dependencies.jar com.interview.kafkaconsumer.KafkaConsumerApp &> logs/kafka-consumer.log &
+    java -classpath bin/kafka-consumer-1.0-SNAPSHOT-jar-with-dependencies.jar com.interview.kafkaconsumer.KafkaConsumerApp &> logs/kafka-consumer.log &
+    echo "$!" > kafkaconsumer.pid
     echo 'Sleeping for 5 seconds' && sleep 5
-    scala -classpath bin/thrift-client-1.0-SNAPSHOT-jar-with-dependencies.jar com.interview.thriftclient.ThriftClientApp &> logs/thrift-client.log &
+    java -classpath bin/thrift-client-1.0-SNAPSHOT-jar-with-dependencies.jar com.interview.thriftclient.ThriftClientApp &> logs/thrift-client.log &
+    echo "$!" > thriftclient.pid
+fi
+
+if [ "$stopApps" -eq "1" ]; then
+	kill $(cat thriftserver.pid) && echo "thriftserver stopped with (pid: $(cat thriftserver.pid))"
+	kill $(cat thriftclient.pid) && echo "thriftclient stopped with (pid: $(cat thriftclient.pid))"
+	kill $(cat kafkaconsumer.pid) && echo "kafkaconsumer stopped with (pid: $(cat kafkaconsumer.pid))"
+	rm ~/generate_random_logs.tmp
 fi
